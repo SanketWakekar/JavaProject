762a972e9acfdecefd3f97340d95365b455bd7a4
91967 <91967@SANKETWAKEKAR>
2020-09-29 16:02:11 +0530

    Initial Commit

---
+++ JavaProject/src/com/example/sanket/MainApp.java
@@ -0,0 +1,13 @@
+package com.example.sanket;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		Student student = new Student("Java","Pune");
+		
+		System.out.println(student);
+
+	}
+
+}
