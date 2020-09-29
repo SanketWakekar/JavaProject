a3dd015ee516c4d0dbfa11f8c7c7f1d6c4d68099
91967 <91967@SANKETWAKEKAR>
2020-09-29 16:09:31 +0530

    Adding new Student object

--- JavaProject/src/com/example/sanket/MainApp.java
+++ JavaProject/src/com/example/sanket/MainApp.java
@@ -5,8 +5,10 @@ public class MainApp {
 	public static void main(String[] args) {
 		
 		Student student = new Student("Java","Pune");
+		Student student1 = new Student();
 		
 		System.out.println(student);
+		System.out.println(student1);
 
 	}
 
